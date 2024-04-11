7ceb26a9122f36d1cac5dda7756f3ab1f6154bc5
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-04-11 19:42:27 +0530

    delete todo functionality

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -2,6 +2,18 @@ import React, { Fragment, useState, useEffect } from "react";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
+
+  // delete todo function
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:5000/todos"); // default fetch makes GET request
@@ -33,10 +45,17 @@ const TodoList = () => {
           </tr> */}
 
           {todos.map((todo) => (
-            <tr key={todo.id}>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete{" "}
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
