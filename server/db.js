a0087034bb39335b4df25139c7fe9636af527d06
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-04-11 16:29:20 +0530

    add comment to pg connection

--- server/db.js
+++ server/db.js
@@ -1,3 +1,13 @@
+/**
+ * Pool is a class from the pg module that allows us to create a connection pool.
+ * A connection pool is a cache of database connections maintained so that the connections
+ * can be reused when future requests to the database are required.
+ *
+ * Creating a pool of connections is more efficient than opening a new connection
+ * for every database query. The configuration object passed to the Pool constructor
+ * contains the credentials and database information required to connect to the PostgreSQL database.
+ */
+
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
