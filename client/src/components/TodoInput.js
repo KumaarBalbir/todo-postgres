9d66c5c9cc4f1f8c2c191c0e058d98259561eb05
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-04-11 19:21:24 +0530

    add component: todo input

---
+++ client/src/components/TodoInput.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useState } from "react";
+
+const TodoInput = () => {
+  const [description, setDescription] = useState("");
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      window.location = "/"; // refreshes the page once the todo is added
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  return (
+    <Fragment>
+      <h1 className="text-center my-5">Pern Todo List</h1>
+
+      <form className="d-flex" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter todo"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success mx-2">Add</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default TodoInput;
