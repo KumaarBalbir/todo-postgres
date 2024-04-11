3b2f1c0b54086cfe7a30a1247e40110fe5cf4d34
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-04-11 20:10:32 +0530

    update: app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 function App() {
   return (
     <Fragment>
       <div className="container">
         <TodoInput />
+        <TodoList />
       </div>
     </Fragment>
   );
