7a807eafecefad4b856f3ff8415c0549a900b76c
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-04-11 16:29:50 +0530

    routes: create a todo

--- server/index.js
+++ server/index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const pool = require("./db");
 
 //middleware
 app.use(cors());
 
 // get client side data from body in json format
-app.use(express.json());
+app.use(express.json()); // req.body
+
+// Routes
+
+// create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// get all todos
+
+// update a todo
+
+// delete a todo
 
 // start server
 app.listen(5000, () => {
