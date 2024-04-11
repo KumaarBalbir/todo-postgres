3b832fa3fb50fb7f9bf610ba2d16213c8270b6f0
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-04-11 19:21:05 +0530

    client initial setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import TodoInput from "./components/TodoInput";
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <TodoInput />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
